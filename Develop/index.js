b913166298c48c7efaa2e0a8edb6f3800bb26a48
nelson509 <35314794+nelson509@users.noreply.github.com>
2022-10-15 17:08:20 -0700

    created array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description'
+    },
+    {
+        type: 'confirm',
+        message: 'Do you have a table of contents?',
+        name: 'table'
+    }, 
+    {
+        type: 'input',
+        message: 'What steps are required to install your application?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use.',
+        name: 'usage'
+    },
+    {
+        type: 'checkbox',
+        message: 'Which license are you planning on using for this project?',
+        choices: ['MIT', 'ISC', 'Apache2.0'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines for this project?',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: '',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github question'
+
+    },
+    {
+        type: 'input',
+        message: 'What is your email for any further questions?',
+        name: 'email question'
+    }
+
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', JSON.stringify(data, null, '\t'),
+        (err) => err ? console.log(err) : console.log("README.md file succsfully created!"));
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        // const fileName = `${data.   .split(' ').join(' ')}.md`;
+        console.log(data);
+    });
+}
+
+
+
 
 // Function call to initialize app
 init();
